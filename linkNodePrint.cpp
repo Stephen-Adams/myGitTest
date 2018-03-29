4a2b4d879bcaf0ce67a5991a3bf21613b3f540ff
stephen <lhd_bit@126.com>
2018-03-29 10:57:18 +0800

    add cpp linkNodePrint.cpp

---
+++ linkNodePrint.cpp
@@ -0,0 +1,25 @@
+/**
+*  struct ListNode {
+*        int val;
+*        struct ListNode *next;
+*        ListNode(int x) :
+*              val(x), next(NULL) {
+*        }
+*  };
+*/
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        int length=0,i;
+        ListNode* p=head;
+        while(p!=nullptr){
+            p=p->next;
+            length++;
+        }
+        vector<int> ans(length);
+        for(i=0,p=head;p!=nullptr;p=p->next,i++){
+            ans[length-1-i]=p->val;
+        }
+        return ans;
+    }
+};
